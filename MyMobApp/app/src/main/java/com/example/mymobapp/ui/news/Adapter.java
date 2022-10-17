8c433dc8f2cd3837d47b297bba195c9f4e0617a1
Ruzica74 <ruzica.dosenovic@student.etf.unibl.org>
2022-10-17 16:47:48 +0200

    news prikazuje se jedan item

---
+++ MyMobApp/app/src/main/java/com/example/mymobapp/ui/news/Adapter.java
@@ -0,0 +1,120 @@
+package com.example.mymobapp.ui.news;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.example.mymobapp.R;
+import com.example.mymobapp.model.Article;
+
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+    private List<Article> articles;
+    private Context context;
+    private OnItemClickListener onItemClickListener;
+
+    public Adapter(List<Article> articles, Context context) {
+        this.articles = articles;
+        this.context = context;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.news_item, parent, false);
+        return new MyViewHolder(view, onItemClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holders, int position) {
+        final MyViewHolder holder=holders;
+        Article model = articles.get(position);
+        System.out.println("position:   "+position);
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.placeholder(Utils.getRandomDrawbleColor());
+        requestOptions.error(Utils.getRandomDrawbleColor());
+        requestOptions.diskCacheStrategy(DiskCacheStrategy.ALL);
+        requestOptions.centerCrop();
+
+        Glide.with(context).load(model.getUrlToImage()).into(holder.image);
+
+        holder.title.setText(model.getTitle());
+        holder.desc.setText(model.getDescription());
+        holder.published_at.setText(Utils.DateFormat(model.getPublishedAt()));
+        holder.author.setText(model.getAuthor());
+        holder.source.setText(model.getSource().getName());
+        holder.time.setText("\u2022"+Utils.DateToTimeFormat(model.getPublishedAt()));
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(View view, int position);
+    }
+
+    private void setOnItemClickListener(OnItemClickListener onItemClickListener){
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView title, desc, author, published_at, source, time;
+        ImageView image;
+        OnItemClickListener onItemClickListener;
+
+
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener onItemClickListener) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.title);
+            desc = itemView.findViewById(R.id.desc);
+            author = itemView.findViewById(R.id.author);
+            published_at = itemView.findViewById(R.id.publishedAt);
+            source = itemView.findViewById(R.id.source);
+            time = itemView.findViewById(R.id.time);
+            image = itemView.findViewById(R.id.img);
+
+            this.onItemClickListener = onItemClickListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(onItemClickListener!=null) {
+                onItemClickListener.onItemClick(view, getAdapterPosition());
+            }
+
+        }
+    }
+}
--- MyMobApp/app/src/main/java/com/example/mymobapp/ui/news/NewsFragment.java
+++ MyMobApp/app/src/main/java/com/example/mymobapp/ui/news/NewsFragment.java
@@ -2,18 +2,23 @@ package com.example.mymobapp.ui.news;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mymobapp.R;
 
@@ -21,13 +26,29 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+
+import com.example.mymobapp.model.Article;
+import com.example.mymobapp.model.News;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NewsFragment extends Fragment {
 
-    private String URL = "https://newsapi.org/v2/top-headlines?country=ie&pageSize=3&apiKey=";
+    private String URL = "https://newsapi.org/v2/top-headlines?country=ie&pageSize=20&apiKey=";
     private String api_key = "97fdc077d1ed4371add402633bfdd34f";
     private NewsViewModel mViewModel;
 
-    TextView txtString;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
+    private String TAG = NewsFragment.class.getSimpleName();
+    private Context thisContext;
+
+    //TextView txtString;
 
     public static NewsFragment newInstance() {
         return new NewsFragment();
@@ -48,6 +69,7 @@ public class NewsFragment extends Fragment {
                 Response response = client.newCall(request).execute();
                 return response.body().string();
             } catch (Exception e) {
+                Toast.makeText(thisContext,"No results", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
             return null;
@@ -56,17 +78,25 @@ public class NewsFragment extends Fragment {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            System.out.println("****Vijestii: "+s);
-            txtString = (TextView) getView().findViewById(R.id.textView1);
-            txtString.setMovementMethod(new ScrollingMovementMethod());
-            txtString.setText(s);
+            Gson gson = new Gson();
+            News news = gson.fromJson(s, News.class);
+            articles = news.getArticles();
+            onConnected();
+            adapter.setArticles(articles);
+            adapter.notifyDataSetChanged();
+
         }
+    }
+
+    public void onConnected(){
+
+
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
+        this.thisContext = container.getContext();
         OkHttpHandler okHttpHandler = new OkHttpHandler();
         okHttpHandler.execute(URL+api_key);
         return inflater.inflate(R.layout.fragment_news, container, false);
@@ -77,7 +107,18 @@ public class NewsFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
+        recyclerView = getView().findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(thisContext);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+        adapter = new Adapter(articles, thisContext);
+        recyclerView.setAdapter(adapter);
+
         // TODO: Use the ViewModel
     }
+
+
+
 
 }
